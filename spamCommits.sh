ecd7810a2109d10c8ed25f460e91906ae8c1d7c3
chozen <moustafa.eldehni@hotmail.com>
2025-04-03 16:45:13 +0200

    Tqt t'as rien vu :)

---
+++ spamCommits.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "Je vais te remplir 10 commits tout de suite !"
+
+
+echo "/* commit inutile */" >> ./public/style/style.css
+git add --all
+git commit -m "Tqt t'as rien vu :)"
+echo "/* commit inutile */" >> ./public/style/style.css
+git add --all
+git commit -m "Tqt t'as rien vu :)"
+echo "/* commit inutile */" >> ./public/style/style.css
+git add --all
+git commit -m "Tqt t'as rien vu :)"
+echo "/* commit inutile */" >> ./public/style/style.css
+git add --all
+git commit -m "Tqt t'as rien vu :)"
+echo "/* commit inutile */" >> ./public/style/style.css
+git add --all
+git commit -m "Tqt t'as rien vu :)"
+echo "/* commit inutile */" >> ./public/style/style.css
+git add --all
+git commit -m "Tqt t'as rien vu :)"
+echo "/* commit inutile */" >> ./public/style/style.css
+git add --all
+git commit -m "Tqt t'as rien vu :)"
+echo "/* commit inutile */" >> ./public/style/style.css
+git add --all
+git commit -m "Tqt t'as rien vu :)"
+echo "/* commit inutile */" >> ./public/style/style.css
+git add --all
+git commit -m "Tqt t'as rien vu :)"
+echo "/* commit inutile */" >> ./public/style/style.css
+git add --all
+git commit -m "Tqt t'as rien vu :)"
