be349a9a3bd673da8bfba9a09b197365e3009d86
chozen <moustafa.eldehni@hotmail.com>
2025-04-03 21:08:09 +0200

    petit test

--- commit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Tqt je gère"
-read -p "Donne moi le nom du commit bg : " commit
-
-git add --all
-git commit -m "$commit"
---
+++ testindex.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo '<!DOCTYPE html>' > test2.html
+echo '<html lang="en">' >> test2.html
+echo '<head>' >> test2.html
+echo '    <meta charset="UTF-8">' >> test2.html
+echo '    <meta name="viewport" content="width=device-width, initial-scale=1.0">' >> test2.html
+echo '    <title>Document</title>' >> test2.html
+echo '</head>' >> test2.html
+echo '<body>' >> test2.html
+echo '</body>' >> test2.html
+echo '</html>' >> test2.html
